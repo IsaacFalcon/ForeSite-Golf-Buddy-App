92bb8e9c571d0425c5fd80bb774d6e3e3b49e4f0
Isaac Falcon <ice6490@gmail.com>
2022-12-07 15:55:39 -0600

    took out not needed bcrypt route, and updated server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3001;
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-  secret: 'secret',
+  secret: process.env.SECRET,
   cookie: {},
   resave: false,
   saveUninitialized: true,
